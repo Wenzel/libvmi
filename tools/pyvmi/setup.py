6041c4d08661fcc367bec41c1507389d716286a6
Mathieu Tarral <mathieu.tarral@gmail.com>
2016-10-06 14:24:00 +0200

    port setup.py to python3

--- tools/pyvmi/setup.py
+++ tools/pyvmi/setup.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python
-import commands
+import subprocess
 from distutils.core import setup, Extension
 
-pkgconfig_cflags = commands.getoutput ("pkg-config --cflags glib-2.0 libvmi")
-pkgconfig_include_flags = commands.getoutput ("pkg-config --cflags-only-I glib-2.0 libvmi")
+pkgconfig_cflags = subprocess.getoutput ("pkg-config --cflags glib-2.0 libvmi")
+pkgconfig_include_flags = subprocess.getoutput ("pkg-config --cflags-only-I glib-2.0 libvmi")
 pkgconfig_include_dirs = [ t[2:] for t in pkgconfig_include_flags.split() ]
-pkgconfig_lflags = commands.getoutput ("pkg-config --libs-only-l glib-2.0 libvmi")
+pkgconfig_lflags = subprocess.getoutput ("pkg-config --libs-only-l glib-2.0 libvmi")
 pkgconfig_libs = [ t[2:] for t in pkgconfig_lflags.split() ]
-pkgconfig_biglflags = commands.getoutput ("pkg-config --libs-only-L glib-2.0 libvmi")
+pkgconfig_biglflags = subprocess.getoutput ("pkg-config --libs-only-L glib-2.0 libvmi")
 pkgconfig_ldirs = [ t[2:] for t in pkgconfig_biglflags.split() ]
 
 pyvmimod = Extension('pyvmi', sources=['pyvmi.c'],
