0aca6dd6a6c6944edcdd3ce2979f812030537cb3
Mathieu Tarral <mathieu.tarral@protonmail.com>
2020-08-09 23:43:45 +0200

    libvmi: remove Xen driver

--- libvmi/driver/driver_interface.c
+++ libvmi/driver/driver_interface.c
@@ -35,10 +35,6 @@
 #include "driver/file/file.h"
 #endif
 
-#ifdef ENABLE_XEN
-#include "driver/xen/xen.h"
-#endif
-
 #ifdef ENABLE_KVM
 #include "driver/kvm/kvm.h"
 #endif
@@ -55,14 +51,6 @@ status_t driver_init_mode(const char *name,
 {
     unsigned long count = 0;
 
-    /* see what systems are accessable */
-#ifdef ENABLE_XEN
-    if (VMI_SUCCESS == xen_test(domainid, name, init_flags, init_data)) {
-        dbprint(VMI_DEBUG_DRIVER, "--found Xen\n");
-        *mode = VMI_XEN;
-        count++;
-    }
-#endif
 #ifdef ENABLE_KVM
     if (VMI_SUCCESS == kvm_test(domainid, name, init_flags, init_data)) {
         dbprint(VMI_DEBUG_DRIVER, "--found KVM\n");
@@ -112,11 +100,6 @@ status_t driver_init(vmi_instance_t vmi,
     bzero(&vmi->driver, sizeof(driver_interface_t));
 
     switch (vmi->mode) {
-#ifdef ENABLE_XEN
-        case VMI_XEN:
-            rc = driver_xen_setup(vmi);
-            break;
-#endif
 #ifdef ENABLE_KVM
         case VMI_KVM:
             rc = driver_kvm_setup(vmi);
